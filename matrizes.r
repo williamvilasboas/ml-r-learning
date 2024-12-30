03f15a34a73efc68c78d20232aab5d2741df7ce8
William Vilas Boas <william.vboas@gmail.com>
2024-12-30 18:46:58 -0300

    Opera

---
+++ matrizes.r
@@ -0,0 +1,13 @@
+print("Trabalhando com matriz")
+
+matrizA <- matrix(c(2, 4, 3, 1, 5, 7), nrow = 2,ncol = 3,byrow = TRUE)
+
+matrizB <- matrix(c(3, 4, 5, 6, 7, 8), nrow = 2, ncol = 3, byrow = TRUE)
+print(matrizA)
+print(matrizB)
+
+print(matrizA * matrizB) # Multiplicação de matrizes
+
+print(matrizA + matrizB) # Soma de Matrizes
+
+print(matrizA / matrizB) # Divisão
--- vetores.r
+++ vetores.r
@@ -3,5 +3,11 @@ print('Trabalhando com vetores')
 primeiroVetor <- c(1, 3, 5, 9, 10)
 
 print(primeiroVetor)
-
 length(primeiroVetor)
+
+segundoVetor <- c(1, 4, 5, 8, 22)
+
+terceiroVetor <- c(primeiroVetor, segundoVetor)
+
+print(terceiroVetor)
+length(terceiroVetor)
