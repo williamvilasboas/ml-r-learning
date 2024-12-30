85c840a860b642d56fccc968fe46cb81ffd05a68
William Vilas Boas <william.vboas@gmail.com>
2024-12-30 17:09:55 -0300

    Comparação de strings

---
+++ operadores-logicos.r
@@ -0,0 +1,24 @@
+print("Trabalhando com operadores logicos")
+
+# X < Y operador de menor
+# X > Y Operador de maior
+# X == Y Operador de igualdade
+# X != Y Operador de diferente
+
+# X >= Y Operador de maior com igualdade
+# X <= Y Operador de menor com igualdade
+
+X <- 10
+Y <- 20
+
+print(X < Y)
+
+
+print("Comparando string")
+
+primeira_letra_nome <- 'W'
+
+primeira_letra_segundo_nome <- 'V'
+
+
+primeira_letra_nome == primeira_letra_segundo_nome
